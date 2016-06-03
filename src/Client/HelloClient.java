202be7d3b211b11024fa92ee733b1c98cd21e51c
FrankYao <570468837@qq.com>
2016-06-03 10:39:03 +0800

    change

--- src/Client/HelloClient.java
+++ src/Client/HelloClient.java
@@ -19,7 +19,7 @@ public class HelloClient {
     public static void main(String args[]){ 
     	HelloClient h = new HelloClient();
         try { 
-            //在RMI服务注册表中查找名称为RHello的对象，并调用其上的方法 
+            //在RMI服务注册表中查找名称为RHello的对象，并调用其上的方法  
             IHello rhello =(IHello) Naming.lookup("rmi://localhost:8888/RHello"); 
             System.out.println(rhello.helloWorld()); 
             System.out.println(rhello.sayHelloToSomeBody("熔岩")); 
